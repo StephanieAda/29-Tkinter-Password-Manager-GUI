3ea259277f37233dbf776daceb0e6f7ea92b852b
Stephanie Elluh <99142577+StephanieAda@users.noreply.github.com>
2022-03-30 01:58:59 +0100

    Finished

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
 
 window = Tk()
 window.title('Password Manager')
@@ -6,12 +9,51 @@ window.config(padx=20, pady=20)
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
+def generate_password():
+
+    password_entry.delete(0, END)
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+    password_list = password_letters + password_numbers + password_symbols
+
+    shuffle(password_list)
+
+    generated_password = "".join(password_list)
+    password_entry.insert(0, generated_password)
+    pyperclip.copy(generated_password)
+
+
 # ---------------------------- SAVE PASSWORD -------------------------------
 
-file = open('Data', 'a')
 def add_data():
-    file.write(f'{website} ! {username} ! {password}')
-    
+    website = website_entry.get()
+    username = username_entry.get()
+    password = password_entry.get()
+
+    # if not website.isalnum() or not password.isalnum() or not username.isalnum():
+    #     print('Yeah')
+    # print("done")
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showwarning(title='Missing Information', message='Some fields are empty!')
+
+    else:
+
+        is_ok = messagebox.askokcancel(title=website, message=f'Are these details correct? \n '
+                                                              f'Email/Username:{username} \n Password: {password}  ')
+        if is_ok:
+            with open('data.txt', 'a') as file:
+                file.write(f'{website} ! {username} ! {password} \n')
+                print('done')
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -37,12 +79,8 @@ password_label = Label(text="Password:")
 password_label.grid(row=4, column=0)
 password_entry = Entry(width=25)
 password_entry.grid(row=4, column=1)
-generate_password = Button(text='Generate Password', width=20)
+generate_password = Button(text='Generate Password', width=20, command=generate_password)
 generate_password.grid(row=4, column=2)
-
-website = website_entry.get()
-username = username_entry.get()
-password = password_entry.get()
 
 add_button = Button(text="Add", width=45, command=add_data)
 add_button.grid(row=5, column=1, columnspan=2)
